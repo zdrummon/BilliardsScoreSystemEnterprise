b4334ed2c34d837154d2626cdbdb3e4b36d8075c
Trevor Haggerty <cesismalon@gmail.com>
2018-01-11 02:30:08 -0800

    Major overhauls to logic, added a log, and formatted / cleaned up code

--- src/BilliardLogicSupport.java
+++ src/BilliardLogicSupport.java
@@ -1,157 +1,226 @@
-public class BilliardLogicSupport {
+import java.util.Date;
 
-	int gameCount = 10;
-	int currentGameNumber;
-	int[] gameWinRecordArray = new int[gameCount];
-	int[] ballOnTableArray= new int[16];
-	int[] ballPocketArray= new int[16];
-	int[] currentInningArray= new int[gameCount];
-	int[] playerOneStats= new int[8];
-	int[] playerTwoStats= new int[8];
-	int currentInning;
-	int turnCounter;
-	int playerOneTargetBalls;
-	int playerTwoTargetBalls;					
-		
-	public BilliardLogicSupport() {
-			gameCount = 10;
+public class BilliardLogicSupport {
+	
+	static Date dateLog = new Date();
+	static int logCounter = 0;
+
+	static int totalGameCount = 10;
+	int currentGameNumber = 0;
+	int[] gameWinRecordArray = new int[totalGameCount];
+	int[][] ballOnTableArray = new int[16][2];
+	int[] currentInningArray = new int[totalGameCount];
+	int[] playerOneStats = new int[8];
+	int[] playerTwoStats = new int[8];
+	int currentInning = 0;
+	int turnCounter = 0;
+	int playerOneTargetBalls = 0;
+	int playerTwoTargetBalls = 0;
+	//WIP
+	boolean[] pocketBallArray = new boolean[16]; 
 			
-		    currentGameNumber = 0;
-		    
-		    for (int i = 0; i < gameCount; i++) {
-		    	gameWinRecordArray[i] = 0;
-		    }			
+	public BilliardLogicSupport() {
+//ARRAY-INITIALIZERS-----------------------------------------------------------------------------------------------------		
+		for (int i = 0; i < gameWinRecordArray.length; i++) {
+			gameWinRecordArray[i] = 0;
+		}
+		
+		for (int j = 0; j < 16; j++) {
+			for (int i = 0; i < 2; i++) {
+				ballOnTableArray[j][i] = 0;
+			}	
+		}
+		
+		for (int i = 0; i < currentInningArray.length; i++) {
+			currentInningArray[i] = 0;
+		}	
+		
+		for (int i = 0; i < playerOneStats.length; i++) {
+			playerOneStats[i] = 0;
+		}	
+		
+		for (int i = 0; i < playerTwoStats.length; i++) {
+			playerTwoStats[i] = 0;
+		}	
+	}
+	
+	public void eventLog(String logEvent) {
+		dateLog = new Date();
+		String logCounterString = String.format("| % 10d|", ++logCounter);
+		System.out.println(dateLog.toString() + logCounterString + logEvent);
 	}
 	
 	public void exitGame() {
-		
+		eventLog(" exitGame method called.");
 	}
 	
 	public void forfeit() {
-		
+		eventLog(" forfeit method called.");
+		//TODO Exit game prompt
+		exitGame();
 	}
 	
 	public void exitSet() {
-		
+		eventLog(" exitSet method called.");
+		//TODO exit set prompt
 	}
 
 	public void clearSetVariables() {
+		eventLog(" clearSetVariables method called.");
+	
+		currentGameNumber = 0;
+		
+		for (int i = 0; i < gameWinRecordArray.length; i++) {
+			gameWinRecordArray[i] = 0;
+		}	
+		
+		for (int i = 0; i < currentInningArray.length; i++) {
+			currentInningArray[i] = 0;
+		}	
+		
+		for (int i = 0; i < playerOneStats.length; i++) {
+			playerOneStats[i] = 0;
+		}	
+		
+		for (int i = 0; i < playerTwoStats.length; i++) {
+			playerTwoStats[i] = 0;
+		}	
 		
 	}
 	
 	public void clearGameVariables() {
+		eventLog(" clearGameVariables method called.");
 		
+		//TODO stat recording
+		
+		for (int j = 0; j < 16; j++) {
+			for (int i = 0; i < 2; i++) {
+				ballOnTableArray[j][i] = 0;
+			}	
+		}
+		
+		playerOneTargetBalls = 0;
+		playerTwoTargetBalls = 0;
+		
+		currentGameNumber++;
 	}
 	
 	public void startSetInstance() {
-		
+		eventLog(" startSetInstance method called.");
+		clearSetVariables();
 	}
 	
 	public void startGameInstance() {
-	
+		eventLog(" startGameInstance method called.");
+		clearGameVariables();
 	}
 	
 	public void breakTriangle() {
-		
+		eventLog(" breakTriangle method called.");
 	}
 	
 	public void modifyBallArray() {
-		
+		eventLog(" modifyBallArray method called.");
 	}
 
 	public void switchTurn() {
-	
+		eventLog(" switchTurn method called.");
 	}
 	
 	public void scratchShot() {
-	
+		eventLog(" scratchShot method called.");
 	}
 	
 	public void foulShot() {
-	
+		eventLog(" foulShot method called.");
 	}
 	
 	public void missShot() {
-		
+		eventLog(" missShot method called.");
 	}
 	
 	public void defShot() {
-		
+		eventLog(" defShot method called.");
 	}
 	
 	public void assignBallTargets() {
-		
+		eventLog(" assignBallTargets method called.");
 	}
 	
 	public void autoLose() {
-		
+		eventLog(" autoLose method called.");
 	}
 	
 	public void callShot() {
-		
+		eventLog(" callShot method called.");
 	}
 	
 	public void cueBall() {
-		
+		eventLog(" cueBall method called.");
 	}
 
 	public void oneBall() {
-		
-	}
+		eventLog(" oneBall method called.");
+/*		if (pocketBallArray[1] == false) {
+			ballOnTableArray[0][1] = 1;
+		} else {
+			ballOnTableArray[0][1] = 0;
+		}
+*/	}
 
 	public void twoBall() {
-		
+		eventLog(" twoBall method called.");
 	}
 
 	public void threeBall() {
-		
+		eventLog(" threeBall method called.");
 	}
 	
 	public void fourBall() {
-		
+		eventLog(" fourBall method called.");
 	}
 	
 	public void fiveBall() {
-		
+		eventLog(" fiveBall method called.");
 	}
 	
 	public void sixBall() {
-		
+		eventLog(" sixBall method called.");	
 	}
 	
 	public void sevenBall() {
-		
+		eventLog(" sevenBall method called.");
 	}
 	
 	public void eightBall() {
-		
+		eventLog(" eightBall method called.");
 	}
 	
 	public void nineBall() {
-		
+		eventLog(" nineBall method called.");
 	}
 	
 	public void tenBall() {
-		
+		eventLog(" tenBall method called.");
 	}
 	
 	public void elevenBall() {
-		
+		eventLog(" elevenBall method called.");
 	}
 	
 	public void twelveBall() {
-		
+		eventLog(" twelveBall method called.");
 	}
 	
 	public void thirteenBall() {
-		
+		eventLog(" thirteenBall method called.");
 	}
 	
 	public void fourteenBall() {
-		
+		eventLog(" fourteenBall method called.");
 	}
 	
 	public void fifteenBall() {
-		
+		eventLog(" fifteenBall method called.");
 	}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 //____________________TITLE______________________________________________________________________________________________
-/*Billiard System v.0003
+/*Billiard System v.0004
 *coded by
 *	Trevor Haggerty - cesismalon@gmail.com
 *	Zachary Drummond - zdrummon@gmail.com
@@ -15,64 +15,45 @@ import javafx.stage.Stage;
 
 //____________________MAIN_______________________________________________________________________________________________
 public class Main extends Application  {
-	
+		
 	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 	int windowHeight = (int) screenSize.getHeight() / 2;
 	int windowWidth = (int) screenSize.getWidth() / 2;
 	
 	int buttonSizeWidth = windowWidth / 8;
 	int buttonSizeHeight = windowHeight / 8;
-		
+	int ballRowHeight = windowHeight / 8 - 15;	
 	//int windowHeight = 600;
 	//int windowWidth = 800;
-						
+			
 	BilliardLogicSupport BSLogic = new BilliardLogicSupport();
-	Stage BSWindow;
+	Stage BSStage;
 	Scene BSInterfaceScene, BSStartScene;
 	
-	public static void main(String[] arg) {
-		launch(arg);		
+	public static void main(String[] arg) {		
+		System.out.println( "          EVENT TIME        |EVENT COUNT|LOG ITEM");
+		launch(arg);	
 	}
 	
 //____________________START______________________________________________________________________________________________
 	@Override
 	public void start(Stage BSStage) throws Exception {
-		BSWindow = BSStage;
-		BSWindow.setResizable(false);
+		BSLogic.eventLog(" launch program");
 		
-		BSWindow.setTitle("Billiard Score System Enterprise - ObserVR 2018");
-	
-		int ballRowHeight = windowHeight / 8 - 15;		
-	
-//-------------------START-BUTTON----------------------------------------------------------------------------------------
-		Button startButton = new Button("click to start score keeping");
-		startButton.setOnAction(e -> BSWindow.setScene(BSInterfaceScene));
-		startButton.setPrefSize(buttonSizeWidth * 2, buttonSizeHeight);
-		startButton.relocate(windowWidth / 2 - buttonSizeWidth, windowHeight / 2 - buttonSizeHeight / 2);
-	
-//-------------------EXIT-BUTTON-----------------------------------------------------------------------------------------	
-		Button exitButton = new Button("click to exit program");
-		exitButton.setOnAction(e ->{
-		BSWindow.close();
-		
-		});
-		exitButton.setPrefSize(buttonSizeWidth * 2, buttonSizeHeight);
-		exitButton.relocate(windowWidth / 2 - buttonSizeWidth, windowHeight / 2 + buttonSizeHeight / 2);
-		
-//-------------------START-SCREEN-LAYOUT---------------------------------------------------------------------------------
+//CONSTRUCTORS-----------------------------------------------------------------------------------------------CONSTRUCTORS
 		Pane BSStartLayout = new Pane();
-		BSStartLayout.getChildren().addAll(startButton,exitButton);
+		Pane BSInterfaceLayout = new Pane();
 		BSStartScene = new Scene(BSStartLayout, windowWidth, windowHeight);
-		BSWindow.setScene(BSStartScene);
-			
-//-------------------SCORE-KEEPING-SCREEN-COMPONENTS---------------------------------------------------------------------	
+		BSInterfaceScene =  new Scene(BSInterfaceLayout, windowWidth, windowHeight);
+		Button startButton = new Button("click to start score keeping");
+		Button exitButton = new Button("click to exit program");
 		Button breakButton = new Button("break");
 		Button defShotButton = new Button("defense");
 		Button ballPocketed = new Button("pocketed");
 		Button missButton = new Button("miss");
 		Button foulButton = new Button("foul");		
 		Button forfeitButton = new Button("forfeit");		
-		Button cueBallButton = new Button("Cue Ball");		
+		Button cueBallButton = new Button("cue Ball");		
 		Button oneBallButton = new Button("1 Ball");		
 		Button twoBallButton = new Button("2 Ball");		
 		Button threeBallButton = new Button("3 Ball");		
@@ -87,122 +68,206 @@ public class Main extends Application  {
 		Button twelveBallButton = new Button("12 Ball");		
 		Button thirteenBallButton = new Button("13 Ball");		
 		Button fourteenBallButton = new Button("14 Ball");
-		Button fifteenBallButton = new Button("15 Ball");					
+		Button fifteenBallButton = new Button("15 Ball");			
+													
+//STAGE-DETAILS---------------------------------------------------------------------------------------------STAGE-DETAILS		
+		BSStage.setResizable(false);
+		BSStage.setTitle("Billiard Score System Enterprise - ObserVR 2018");
+					
+//START-BUTTON-----------------------------------------------------------------------------------------------START-BUTTON
+		startButton.setPrefSize(buttonSizeWidth * 2, buttonSizeHeight);
+		startButton.relocate(windowWidth / 2 - buttonSizeWidth, windowHeight / 2 - buttonSizeHeight / 2);
+		startButton.setOnAction(e -> {
+			BSLogic.eventLog(" start button pressed");
+			BSStage.setScene(BSInterfaceScene);
+			BSLogic.startSetInstance();
+		});
 		
-//-------------------BREAK-BUTTON----------------------------------------------------------------------------------------
-		breakButton.setOnAction(e ->{
+//EXIT-BUTTON-------------------------------------------------------------------------------------------------EXIT-BUTTON
+		exitButton.setPrefSize(buttonSizeWidth * 2, buttonSizeHeight);
+		exitButton.relocate(windowWidth / 2 - buttonSizeWidth, windowHeight / 2 + buttonSizeHeight / 2);
+		exitButton.setOnAction(e -> {
+			BSLogic.eventLog(" exit button pressed");
+			BSStage.close();
+		});
+				
+//START-SCREEN-LAYOUT---------------------------------------------------------------------------------START-SCREEN-LAYOUT
+		BSStartLayout.getChildren().addAll(startButton, exitButton);
+		BSStage.setScene(BSStartScene);
+			
+//SCORE-KEEPING-SCREEN-COMPONENTS---------------------------------------------------------SCORE-KEEPING-SCREEN-COMPONENTS
+														//  @  \\
+//BREAK-BUTTON-----------------------------------------------------------------------------------------------BREAK-BUTTON
+		breakButton.setPrefSize(buttonSizeWidth, buttonSizeHeight);
+		breakButton.relocate(windowWidth / 2 - buttonSizeWidth / 2, windowHeight / 2 - buttonSizeHeight * 5 / 2 - 10);
+		breakButton.setOnAction(e -> {
+			BSLogic.eventLog(" break button pressed.");
 			BSLogic.breakTriangle();
 			breakButton.setVisible(false);
 			defShotButton.setVisible(true);
 		});
-		breakButton.setPrefSize(buttonSizeWidth, buttonSizeHeight);
-		breakButton.relocate(windowWidth / 2 - buttonSizeWidth / 2, windowHeight / 2 - buttonSizeHeight * 5 / 2 - 10);
 		
-//-------------------BALL-POCKETED---------------------------------------------------------------------------------------		
-		ballPocketed.setOnAction(e ->{
-			BSLogic.modifyBallArray();
-		});
-
+//BALL-POCKETED---------------------------------------------------------------------------------------------BALL-POCKETED
 		ballPocketed.setPrefSize(buttonSizeWidth, buttonSizeHeight);
 		ballPocketed.relocate(windowWidth / 2 - buttonSizeWidth / 2, windowHeight / 2 - buttonSizeHeight * 3 / 2 - 10);
-				
-//-------------------DEFENSIVE-SHOT-BUTTON-------------------------------------------------------------------------------
-		defShotButton.setVisible(false);
-		defShotButton.setOnAction(e -> BSLogic.defShot());
+		ballPocketed.setOnAction(e -> {
+			BSLogic.eventLog(" ball pocketed button pressed.");
+			BSLogic.modifyBallArray();
+		});	
+		
+//DEFENSIVE-SHOT-BUTTON-----------------------------------------------------------------------------DEFENSIVE-SHOT-BUTTON
 		defShotButton.setPrefSize(buttonSizeWidth, buttonSizeHeight);
 		defShotButton.relocate(windowWidth / 2 - buttonSizeWidth / 2, windowHeight / 2 - buttonSizeHeight / 2 - 10);
-
-//-------------------MISS-BUTTON-----------------------------------------------------------------------------------------
-		missButton.setOnAction(e -> BSLogic.missShot());
+		defShotButton.setVisible(false);
+		defShotButton.setOnAction(e -> {
+			BSLogic.eventLog(" defense shot button pressed.");
+			BSLogic.defShot();
+		});
+		
+//MISS-BUTTON-------------------------------------------------------------------------------------------------MISS-BUTTON
 		missButton.setPrefSize(buttonSizeWidth, buttonSizeHeight);
 		missButton.relocate(windowWidth / 2 - buttonSizeWidth / 2, windowHeight / 2 + buttonSizeHeight / 2 - 10);
+		missButton.setOnAction(e -> {
+			BSLogic.eventLog(" missed shot button pressed.");
+			BSLogic.missShot();
+		});
 	
-//-------------------FOUL-BUTTON-----------------------------------------------------------------------------------------
-		foulButton.setOnAction(e -> BSLogic.foulShot());
+//FOUL-BUTTON-------------------------------------------------------------------------------------------------FOUL-BUTTON
 		foulButton.setPrefSize(buttonSizeWidth, buttonSizeHeight);
 		foulButton.relocate(windowWidth / 2 - buttonSizeWidth / 2, windowHeight / 2 + buttonSizeHeight * 3 / 2 - 10);
+		foulButton.setOnAction(e -> {
+			BSLogic.eventLog(" foul button pressed.");
+			BSLogic.foulShot();
+		});
 			
-//-------------------FORFEIT-BUTTON--------------------------------------------------------------------------------------
-		forfeitButton.setOnAction(e -> BSLogic.forfeit());
+//FORFEIT-BUTTON-------------------------------------------------------------------------------------------FORFEIT-BUTTON
 		forfeitButton.setPrefSize(buttonSizeWidth / 2, buttonSizeHeight / 2);
 		forfeitButton.relocate(windowWidth / 2 - buttonSizeWidth / 4, windowHeight - buttonSizeHeight / 2 - 10);
+		forfeitButton.setOnAction(e -> {
+			BSLogic.eventLog(" forfeit button pressed.");
+			BSLogic.forfeit();
+		});
 		
-//-------------------BALL-BUTTONS----------------------------------------------------------------------------------------	
-		cueBallButton.setOnAction(e -> BSLogic.cueBall());
+//-------------------BALL-BUTTONS----------------------------------------------------------------------------------------
 		cueBallButton.setPrefSize(buttonSizeWidth / 2, buttonSizeHeight / 2);
 		cueBallButton.relocate(windowWidth / 2 - buttonSizeWidth / 4, ballRowHeight - buttonSizeHeight / 2 - 10);
-
-		oneBallButton.setOnAction(e -> BSLogic.oneBall());
+		cueBallButton.setOnAction(e -> { 
+			BSLogic.eventLog(" cue ball button pressed.");
+			BSLogic.cueBall();
+		});
+		
 		oneBallButton.setPrefSize(buttonSizeWidth / 2, buttonSizeHeight / 2);
 		oneBallButton.relocate(windowWidth / 2 - (windowWidth / 16 * 7) - buttonSizeWidth / 4 - 10, ballRowHeight);
+		oneBallButton.setOnAction(e -> {
+			BSLogic.eventLog(" 1 ball button pressed.");
+			BSLogic.oneBall();
+		});
 
-		twoBallButton.setOnAction(e -> BSLogic.twoBall());
 		twoBallButton.setPrefSize(buttonSizeWidth / 2, buttonSizeHeight / 2);
 		twoBallButton.relocate(windowWidth / 2 - (windowWidth / 16 * 6) - buttonSizeWidth / 4 - 10, ballRowHeight);
+		twoBallButton.setOnAction(e -> {
+			BSLogic.eventLog(" 2 ball button pressed.");
+			BSLogic.twoBall();
+		});
 
-		threeBallButton.setOnAction(e -> BSLogic.threeBall());
 		threeBallButton.setPrefSize(buttonSizeWidth / 2, buttonSizeHeight / 2);
 		threeBallButton.relocate(windowWidth / 2 - (windowWidth / 16 * 5) - buttonSizeWidth / 4 - 10, ballRowHeight);
+		threeBallButton.setOnAction(e -> {
+			BSLogic.eventLog(" 3 ball button pressed.");
+			BSLogic.threeBall();
+		});
 		
-		fourBallButton.setOnAction(e -> BSLogic.fourBall());
 		fourBallButton.setPrefSize(buttonSizeWidth / 2, buttonSizeHeight / 2);
-		fourBallButton.relocate(windowWidth / 2 - (windowWidth / 16 * 4) - buttonSizeWidth / 4 - 10, ballRowHeight);		
+		fourBallButton.relocate(windowWidth / 2 - (windowWidth / 16 * 4) - buttonSizeWidth / 4 - 10, ballRowHeight);	
+		fourBallButton.setOnAction(e -> {
+			BSLogic.eventLog(" 4 ball button pressed.");
+			BSLogic.fourBall();
+		});
 
-		fiveBallButton.setOnAction(e -> BSLogic.fiveBall());
 		fiveBallButton.setPrefSize(buttonSizeWidth / 2, buttonSizeHeight / 2);
-		fiveBallButton.relocate(windowWidth / 2 - (windowWidth / 16 * 3) - buttonSizeWidth / 4 - 10, ballRowHeight);		
+		fiveBallButton.relocate(windowWidth / 2 - (windowWidth / 16 * 3) - buttonSizeWidth / 4 - 10, ballRowHeight);	
+		fiveBallButton.setOnAction(e -> {
+			BSLogic.eventLog(" 5 ball button pressed.");
+			BSLogic.fiveBall();
+		});
 
-		sixBallButton.setOnAction(e -> BSLogic.sixBall());
 		sixBallButton.setPrefSize(buttonSizeWidth / 2, buttonSizeHeight / 2);
 		sixBallButton.relocate(windowWidth / 2 - (windowWidth / 16 * 2) - buttonSizeWidth / 4 - 10, ballRowHeight);		
+		sixBallButton.setOnAction(e -> {
+			BSLogic.eventLog(" 6 ball button pressed.");
+			BSLogic.sixBall();
+		});
 
-		sevenBallButton.setOnAction(e -> BSLogic.sevenBall());
 		sevenBallButton.setPrefSize(buttonSizeWidth / 2, buttonSizeHeight / 2);
 		sevenBallButton.relocate(windowWidth / 2 - (windowWidth / 16) - buttonSizeWidth / 4 - 10, ballRowHeight);		
+		sevenBallButton.setOnAction(e -> {
+			BSLogic.eventLog(" 7 ball button pressed.");
+			BSLogic.sevenBall();
+		});
 
-		eightBallButton.setOnAction(e -> BSLogic.eightBall());
 		eightBallButton.setPrefSize(buttonSizeWidth / 2, buttonSizeHeight / 2);
 		eightBallButton.relocate(windowWidth / 2 - buttonSizeWidth / 4, ballRowHeight);		
+		eightBallButton.setOnAction(e -> {
+			BSLogic.eventLog(" 8 ball button pressed.");
+			BSLogic.eightBall();
+		});
 
-		nineBallButton.setOnAction(e -> BSLogic.nineBall());
 		nineBallButton.setPrefSize(buttonSizeWidth / 2, buttonSizeHeight / 2);
 		nineBallButton.relocate(windowWidth / 2 + (windowWidth / 16) - buttonSizeWidth / 4 + 10, ballRowHeight);		
+		nineBallButton.setOnAction(e -> {
+			BSLogic.eventLog(" 9 ball button pressed.");
+			BSLogic.nineBall();
+		});
 
-		tenBallButton.setOnAction(e -> BSLogic.tenBall());
 		tenBallButton.setPrefSize(buttonSizeWidth / 2, buttonSizeHeight / 2);
 		tenBallButton.relocate(windowWidth / 2 + (windowWidth / 16 * 2) - buttonSizeWidth / 4 + 10, ballRowHeight);		
+		tenBallButton.setOnAction(e -> {
+			BSLogic.eventLog(" 10 ball button pressed.");
+			BSLogic.tenBall();
+		});
 
-		elevenBallButton.setOnAction(e -> BSLogic.elevenBall());
 		elevenBallButton.setPrefSize(buttonSizeWidth / 2, buttonSizeHeight / 2);
-		elevenBallButton.relocate(windowWidth / 2 + (windowWidth / 16 * 3) - buttonSizeWidth / 4 + 10, ballRowHeight);		
+		elevenBallButton.relocate(windowWidth / 2 + (windowWidth / 16 * 3) - buttonSizeWidth / 4 + 10, ballRowHeight);	
+		elevenBallButton.setOnAction(e -> {
+			BSLogic.eventLog(" 11 ball button pressed.");
+			BSLogic.elevenBall();
+		});
 
-		twelveBallButton.setOnAction(e -> BSLogic.twelveBall());
 		twelveBallButton.setPrefSize(buttonSizeWidth / 2, buttonSizeHeight / 2);
-		twelveBallButton.relocate(windowWidth / 2 + (windowWidth / 16 * 4) - buttonSizeWidth / 4 + 10, ballRowHeight);		
+		twelveBallButton.relocate(windowWidth / 2 + (windowWidth / 16 * 4) - buttonSizeWidth / 4 + 10, ballRowHeight);	
+		twelveBallButton.setOnAction(e -> {
+			BSLogic.eventLog(" 12 ball button pressed.");
+			BSLogic.twelveBall();
+		});
 
-		thirteenBallButton.setOnAction(e -> BSLogic.thirteenBall());
 		thirteenBallButton.setPrefSize(buttonSizeWidth / 2, buttonSizeHeight / 2);
 		thirteenBallButton.relocate(windowWidth / 2 + (windowWidth / 16 * 5) - buttonSizeWidth / 4 + 10, ballRowHeight);
+		thirteenBallButton.setOnAction(e -> {
+			BSLogic.eventLog(" 13 ball button pressed.");
+			BSLogic.thirteenBall();
+		});
 			
-		fourteenBallButton.setOnAction(e -> BSLogic.fourteenBall());
 		fourteenBallButton.setPrefSize(buttonSizeWidth / 2, buttonSizeHeight / 2);
-		fourteenBallButton.relocate(windowWidth / 2 + (windowWidth / 16 * 6) - buttonSizeWidth / 4 + 10, ballRowHeight);		
+		fourteenBallButton.relocate(windowWidth / 2 + (windowWidth / 16 * 6) - buttonSizeWidth / 4 + 10, ballRowHeight);
+		fourteenBallButton.setOnAction(e -> {
+			BSLogic.eventLog(" 14 ball button pressed.");
+			BSLogic.fourteenBall();
+		});
 
-		fifteenBallButton.setOnAction(e -> BSLogic.fifteenBall());		
 		fifteenBallButton.setPrefSize(buttonSizeWidth / 2, buttonSizeHeight / 2);
 		fifteenBallButton.relocate(windowWidth / 2 + (windowWidth / 16 * 7) - buttonSizeWidth / 4 + 10, ballRowHeight);
+		fifteenBallButton.setOnAction(e -> {
+			BSLogic.eventLog(" 15 ball button pressed.");
+			BSLogic.fifteenBall();
+		});		
 		
-//-------------------SCORE-KEEPING-SCREEN-LAYOUT-------------------------------------------------------------------------
-		Pane BSInterfaceLayout = new Pane();
-		
+//-------------------SCORE-KEEPING-SCREEN-LAYOUT-------------------------------------------------------------------------		
 		BSInterfaceLayout.getChildren().addAll( breakButton, foulButton, missButton, defShotButton,
 												forfeitButton, ballPocketed, cueBallButton, oneBallButton, 
 												twoBallButton, threeBallButton, fourBallButton, fiveBallButton,
 												sixBallButton, sevenBallButton, eightBallButton, nineBallButton,
 												tenBallButton, elevenBallButton, twelveBallButton,
-												thirteenBallButton, fourteenBallButton, fifteenBallButton);
-		BSInterfaceScene =  new Scene(BSInterfaceLayout, windowWidth, windowHeight);
-				
+												thirteenBallButton, fourteenBallButton, fifteenBallButton);				
 		BSStage.show(); 
 	}	
 }
